00073b4bb9cd72b03b27a50b5832fd226a73c38d
vovakpro13 <vovakpro13@gmail.com>
2021-03-22 23:10:31 +0200

    Homework #7 JS Task 1

---
+++ JS (task #1)/task1.js
@@ -0,0 +1,347 @@
+// ща буде тріндец...
+
+// Task 1
+
+//variables
+
+let hello = 'hello';
+let owu =  'owu';
+let com_ =  'com';
+let ua =  'ua';
+let one =  1;
+let ten =  10;
+let neg_nine =  -999;
+let oth =  123;
+let pi =  3.14;
+let ts =  2.7;
+let sixteen =  16;
+let truth =  true;
+let fs =  false;
+
+// console.log()
+
+console.log(hello);
+console.log(owu);
+console.log(com_);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(neg_nine);
+console.log(oth);
+console.log(pi);
+console.log(ts);
+console.log(sixteen);
+console.log(truth);
+console.log(fs);
+
+// alert()
+
+// alert(hello);
+// alert(owu);
+// alert(com_);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(neg_nine);
+// alert(oth);
+// alert(pi);
+// alert(ts);
+// alert(sixteen);
+// alert(truth);
+// alert(fs);
+
+// document.write()
+
+document.write(hello);
+document.write(owu);
+document.write(com_);
+document.write(ua);
+document.write(one);
+document.write(ten);
+document.write(neg_nine);
+document.write(oth);
+document.write(pi);
+document.write(ts);
+document.write(sixteen);
+document.write(truth);
+document.write(fs);
+
+// Task 2
+
+// redefine
+
+hello = 'hi';
+owu =  true;
+com_ =  'comcomcomcom';
+ua =  'ukraine';
+one =  111;
+ten =  'ten';
+neg_nine = '9';
+oth =  0;
+pi =  'number pi';
+ts =  'ts';
+sixteen =  161616;
+truth =  1;
+fs =  0;
+
+// console.log()
+
+console.log(hello);
+console.log(owu);
+console.log(com_);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(neg_nine);
+console.log(oth);
+console.log(pi);
+console.log(ts);
+console.log(sixteen);
+console.log(truth);
+console.log(fs);
+
+// alert()
+
+alert(hello);
+alert(owu);
+alert(com_);
+alert(ua);
+alert(one);
+alert(ten);
+alert(neg_nine);
+alert(oth);
+alert(pi);
+alert(ts);
+alert(sixteen);
+alert(truth);
+alert(fs);
+
+// document.write()
+
+document.write(hello);
+document.write(owu);
+document.write(com_);
+document.write(ua);
+document.write(one);
+document.write(ten);
+document.write(neg_nine);
+document.write(oth);
+document.write(pi);
+document.write(ts);
+document.write(sixteen);
+document.write(truth);
+document.write(fs);
+
+// Task 3
+
+// number constants
+
+const c_ = 299792458;
+const T = -273.15;
+const g = 9.8;
+
+// string constants
+
+const me = 'Volodymyr';
+const mySchool = 'Okten';
+const myAge = 17;
+
+// console.log()
+
+console.log(c_);
+console.log(T);
+console.log(g);
+console.log(me);
+console.log(mySchool);
+console.log(myAge);
+
+// alert()
+
+alert(c);
+alert(T);
+alert(g);
+alert(me);
+alert(mySchool);
+alert(myAge);
+
+// document.write()
+
+document.write(c);
+document.write(T);
+document.write(g);
+document.write(me);
+document.write(mySchool);
+document.write(myAge);
+
+// Task 4
+
+// get PIB
+
+let my_name = prompt("Ваше ім'я: ");
+let my_last_name = prompt("Ваша фамілія: ");
+let my_father_name = prompt("По батькові: ");
+
+// console.log()
+
+console.log(my_name);
+console.log(my_last_name);
+console.log(my_father_name);
+
+// alert()
+
+alert(my_name);
+alert(my_last_name);
+alert(my_father_name);
+
+// document.write()
+
+document.write(my_name);
+document.write(my_last_name);
+document.write(my_father_name);
+
+// Task 5
+
+let person = name + ' ' + last_name + ' ' + father_name;
+
+// Task 6
+
+// father
+let father_name = prompt("Ім'я тата: ");
+let father_last_name = prompt("Фамілія тата: ");
+let father_father_name = prompt("По батькові тата: ");
+let person_father = father_name + ' ' + father_last_name + ' ' + father_father_name;
+
+console.log(father_name);
+console.log(father_last_name);
+console.log(father_father_name);
+
+alert(father_name);
+alert(father_last_name);
+alert(father_father_name);
+
+document.write(father_name);
+document.write(father_last_name);
+document.write(father_father_name);
+
+// mother
+let mother_name = prompt("Ім'я мами: ");
+let mother_last_name = prompt("Фамілія мами: ");
+let mother_father_name = prompt("По батькові маму: ");
+let person_mother = mother_name + ' ' + mother_last_name + ' ' + mother_father_name;
+
+console.log( mother_name);
+console.log( mother_last_name);
+console.log( mother_father_name);
+
+alert( mother_name);
+alert( mother_last_name);
+alert( mother_father_name);
+
+document.write( mother_name);
+document.write( mother_last_name);
+document.write( mother_father_name);
+
+// grandmother
+let grandmother_name = prompt("Ім'я тата: ");
+let grandmother_last_name = prompt("Фамілія тата: ");
+let grandmother_father_name = prompt("По батькові тата: ");
+let person_grandmother = grandmother_name + ' ' + grandmother_last_name + ' ' + grandmother_father_name;
+
+console.log(grandmother_name);
+console.log(grandmother_last_name);
+console.log(grandmother_father_name);
+
+alert(grandmother_name);
+alert(grandmother_last_name);
+alert(grandmother_father_name);
+
+document.write(grandmother_name);
+document.write(grandmother_last_name);
+document.write(grandmother_father_name);
+
+// Task 7
+
+console.log(+prompt("Number 1: "));
+console.log(+prompt("Number 2: "));
+console.log(+prompt("Number 3: "));
+
+// Task 8
+
+let num1 = parseInt(prompt("Number 1: "));
+let num2 = parseInt(prompt("Number 2: "));
+let num3 = parseInt(prompt("Number 3: "));
+let num4= parseInt(prompt("Number 4: "));
+let result = num1 + num2 + num3 + num4;
+
+console.log(result);
+
+// Task 9
+
+let float_num1 = parseFloat(prompt("Float Number 1: "));
+let float_num2 = parseFloat(prompt("Float Number 2: "));
+let float_num3 = parseFloat(prompt("Float Number 3: "));
+result = float_num1 + float_num2 + float_num3;
+
+console.log(result);
+
+// Task 10
+
+let int_num1 = Math.round(parseFloat(prompt("Float Number 1: ")));
+let int_num2 = Math.round(parseFloat(prompt("Float Number 2: ")));
+let int_num3 = Math.round(parseFloat(prompt("Float Number 3: ")));
+
+result = int_num1 + int_num2 + int_num3;
+console.log(result);
+
+// Task 11
+
+let an = +prompt("Число: ");
+let n = +prompt("Степінь: ");
+
+console.log(Math.pow(an, n));
+
+// Task 12
+
+let a = 100;
+let b = '100';
+let c = true;
+let d = undefined;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+// Task 13
+
+let  p = 5 < 6;
+let  p1 = 5 > 6;
+let  p2 = 5 === 6;
+let  p3 = 5 == 6;
+let  p4 = 10 === 10;
+let  p5 = 10 >= 10;
+let  p6 = 10 !== 10;
+let  p7 = 10 < 10;
+let  p8 = 10 > 10;
+let  p9 = 123 === '123';
+let  p10 = 123 == '123';
+
+// Bonus
+
+console.log(132 > 100 && 45 < 12 ); // false
+console.log(34 > 33 && 23 < 90 );   // true
+console.log(99 > 100 && 45 > 12 );  // false
+console.log(132 > 100 || 45 < 12 ); // true
+console.log(111 > 11 || 45 < 111 ); // true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );  // true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );   // false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));  // true
+console.log(!!'-1');    //true
+console.log(!!-1);      //true
+console.log(!!'0');     //true
+console.log(!!'null');  //true
+console.log(!!'undefined'); //true
+console.log(!!(3/'owu'));   //NaN
+console.log((111 > 11 || 45 < 111) ||  !!'0'); // true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); //false
